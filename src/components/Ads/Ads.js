57c956649d51f21e513206e8162205b2f2103f77
unknown <jeremylscott@gmail.com>
2019-03-27 12:37:47 -0500

    First commit

---
+++ src/components/Ads/Ads.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './ads.css'
+import ads from '../../images/advert.png'
+
+
+const Ads = () => {
+
+    return (
+        <>
+            <div className='ad-background'>
+                <img className='ad' src={ads}/>
+            </div>
+            <div className='gradient'/>
+        </>
+
+
+        
+    )
+}
+
+export default Ads
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+
+
+    return (
+        <div className='background'>
+            <img className='ad' src={ad}/>
+        </div>
+
+
+    )
+
+
+
+}
+
+export default Navbar
