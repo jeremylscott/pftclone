34d65f4e0a28ec17702115437718209ae22bc5b3
unknown <jeremylscott@gmail.com>
2019-03-28 10:37:23 -0500

    Additions made to clone

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,15 +7,10 @@ import pftlive from '../../images/pftlive.png'
  class Navbar extends Component {
     constructor() {
         super()
-
-        this.state = {
-            topLinks: ['PFT','NCAA FB','MLB','NBA','NHL','NASCAR','NCAA BB','MOTORS','SOCCER','GOLF'],
-            lowerLinks: ['Home','Latest News & Rumors','FMIA Archive','Teams','Rotoworld','About','PFT Gear','Tickets']
-        }
-
     }
 
     render() {
+
         return (
             <>
                 <div className='navbar-background'>
@@ -23,6 +18,32 @@ import pftlive from '../../images/pftlive.png'
                         <img className='nbc' src={nbc}/>
                         <img className='pft' src={pft}/>
                         <img className='pftlive' src={pftlive}/>
+                    </div>
+                    <div className='top'>
+                        <div className='top-links'>
+                            <li className='tLink1'>PFT</li>
+                            <li className='tLink2'>NCAA FB</li>
+                            <li className='tLink3'>MLB</li>
+                            <li className='tLink4'>NBA</li>
+                            <li className='tLink5'>NHL</li>
+                            <li className='tLink6'>NASCAR</li>
+                            <li className='tLink7'>NCAA BB</li>
+                            <li className='tLink8'>MOTORS</li>
+                            <li className='tLink9'>SOCCER</li>
+                            <li className='tLink10'>GOLF</li>
+                        </div>
+                    </div>
+                    <div className='lower'>
+                        <div className='lower-links'>
+                            <li className='lLink1'>Home</li>
+                            <li className='lLink2'>Latest News & Rumors</li>
+                            <li className='lLink3'>FMIA Archive</li>
+                            <li className='lLink4'>Teams</li>
+                            <li className='lLink5'>Rotoworld</li>
+                            <li className='lLink6'>About</li>
+                            <li className='lLink7'>PFT Gear</li>
+                            <li className='lLink8'>Tickets</li>
+                        </div>
                     </div>
                 </div>
             </>
